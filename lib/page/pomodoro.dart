31d2f934e7eb43d2762a93edc05ae836b320203c
Mariana Oliveira <marianaoliveirafernandes19@gmail.com>
2021-11-03 16:39:15 -0300

    finalizado

--- lib/page/pomodoro.dart
+++ lib/page/pomodoro.dart
@@ -1,4 +1,8 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
+import 'package:flutter/painting.dart';
+import 'package:percent_indicator/percent_indicator.dart';
 
 class Pomodoro extends StatefulWidget {
   const Pomodoro({Key? key}) : super(key: key);
@@ -13,6 +17,35 @@ class _PomodoroState extends State<Pomodoro> {
   static int TimeIntMinut = 25;
   int TimeInSec = TimeIntMinut * 60;
 
+  late Timer timer;
+
+  _StartTimer() {
+    TimeIntMinut = 25;
+    int Time = TimeIntMinut * 60;
+    double SecPercent = (Time/100);
+
+    timer = Timer.periodic(Duration(seconds: 1), (timer) {
+      setState(() {
+        if(Time > 0){
+          Time--;
+          if(Time % 60 == 0){
+            TimeIntMinut--;
+          }if(Time % SecPercent == 0){
+            if(percent < 1){
+              percent += 0.01;
+            }else {
+              percent = 1;
+            }
+          }
+        }else {
+          percent = 0;
+          TimeIntMinut = 25;
+          timer.cancel();
+        }
+      });
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return SafeArea(
@@ -23,6 +56,120 @@ class _PomodoroState extends State<Pomodoro> {
               colors: [Color(0xff1542bf),Color(0xff51a8ff)],
               begin: FractionalOffset(0.5,1)
             )
+          ),
+          width: double.infinity,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Padding(
+                padding: EdgeInsets.only(top: 25.0),
+                child: Text(
+                  "Relógio Pomodoro",
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 40.0,
+                  ),
+                ),
+              ),
+              Expanded(
+                child: CircularPercentIndicator(
+                  percent: percent,
+                  animation: true,
+                  animateFromLastPercent: true,
+                  radius: 250.0,
+                  lineWidth: 20.0,
+                  progressColor: Colors.white,
+                  center: Text(
+                    "$TimeIntMinut",
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 80.0,
+                    ),
+                  ),
+                ),
+              ),
+              SizedBox(height: 30.0,),
+              Expanded(
+                child: Container(
+                  width: double.infinity,
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.only(topRight: Radius.circular(30.0), topLeft: Radius.circular(30.0))
+                  ),
+                  child: Padding(
+                    padding: EdgeInsets.only(top: 30.0, left: 20.0, right: 20.0),
+                    child: Column(
+                      children: [
+                        Expanded(
+                          child: Row(
+                            children: [
+                              Expanded(
+                                child: Column(
+                                  children: [
+                                    Text(
+                                      "Tempo de estudo",
+                                      style: TextStyle(
+                                        fontSize: 30.0,
+                                      ),
+                                    ),
+                                    SizedBox(height: 10.0,),
+                                    Text(
+                                      "25",
+                                      style: TextStyle(
+                                        fontSize: 80.0,
+                                      ),
+                                    )
+                                  ],
+                                ),
+                              ),
+                              Expanded(
+                                child: Column(
+                                  children: [
+                                    Text(
+                                      "Tempo de pausa",
+                                      style: TextStyle(
+                                        fontSize: 30.0,
+                                      ),
+                                    ),
+                                    SizedBox(height: 10.0,),
+                                    Text(
+                                      "5",
+                                      style: TextStyle(
+                                        fontSize: 80.0,
+                                      ),
+                                    )
+                                  ],
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                        Padding(
+                          padding: EdgeInsets.symmetric(vertical: 28.0),
+                          child: RaisedButton(
+                            onPressed: _StartTimer,
+                            color: Colors.blue,
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(100.0),
+                            ),
+                            child: Padding(
+                              padding: EdgeInsets.all(20.0),
+                              child: Text("começar a estudar",
+                                style: TextStyle(
+                                color: Colors.white,
+                                  fontSize: 22.0
+                              ),
+                              ),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+            ],
           ),
         ),
       ),
